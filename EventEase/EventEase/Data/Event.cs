4a168a4ba622089f9fafd59ea88c68f9d6460250
Steven Moya <steven.moya280400@outlook.com>
2025-04-08 21:11:11 -0600

    Actividad 1

---
+++ EventEase/EventEase/Data/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace EventEase.Data
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Location { get; set; } = string.Empty;
+    }
+}
