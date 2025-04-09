c89a3eef9d80ee63a7f49c3c36839fd6ea977fcb
Steven Moya <steven.moya280400@outlook.com>
2025-04-08 22:05:01 -0600

    Actividad 3

---
+++ EventEase/EventEase/Data/EventService.cs
@@ -0,0 +1,22 @@
+﻿namespace EventEase.Data
+{
+    public class EventService
+    {
+        private List<Event> allEvents = new()
+        {
+            new Event { Id = 1, Name = "Tech Conference 2025", Date = new DateTime(2025, 5, 15), Location = "San José" },
+            new Event { Id = 2, Name = "Summer Gala", Date = new DateTime(2025, 6, 1), Location = "Cartago" },
+            new Event { Id = 3, Name = "Developer Meetup", Date = new DateTime(2025, 6, 20), Location = "Heredia" }
+        };
+
+        public Task<List<Event>> GetEventsAsync()
+        {
+            return Task.FromResult(allEvents);
+        }
+
+        public Task<Event?> GetEventByIdAsync(int id)
+        {
+            return Task.FromResult(allEvents.FirstOrDefault(x=>x.Id==id));
+        }
+    }
+}
---
+++ EventEase/EventEase/Data/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEase.Data
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name can't be longer than 100 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+    }
+}
